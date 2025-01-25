e899ff55fb4f2b4b6b0ae8f6ca5ca322510e8a15
HyunwooKim04 <155235090+HyunwooKim04@users.noreply.github.com>
2025-01-25 22:50:29 +0900

    Update 1546.cpp

--- 1546.cpp
+++ 1546.cpp
@@ -6,15 +6,17 @@ int main() {
     double max = 0;
     std::cin >> N;
     double* score = new double[N];
+    
     for(int i=0; i<N; i++) {
         std::cin >> score[i];
         if(score[i] > max)
             max = score[i];
     }
+    
     for(int i=0; i<N; i++) {
         score[i] = score[i] / max * 100;
         average += score[i];
     }
     
-    std::cout << average / N;
+    std::cout << average / N << '\n';
 }
